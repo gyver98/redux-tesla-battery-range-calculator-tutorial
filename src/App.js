69672f998aa1acae43bc2c04b923bde467c3c459
gyver98 <matthewchoi7@gmail.com>
2017-03-17 15:43:54 +1100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import TeslaCarContainer from './containers/TeslaCarContainer';
-import TeslaStatsContainer from './containers/TeslaStatsContainer';
-import TeslaSpeedCounterContainer from './containers/TeslaSpeedCounterContainer';
-import TeslaTempCounterContainer from './containers/TeslaTempCounterContainer';
-import TeslaClimateContainer from './containers/TeslaClimateContainer';
-import TeslaWheelsContainer from './containers/TeslaWheelsContainer';
-import TeslaNotice from './components/TeslaNotice/TeslaNotice';
-import Header from './components/Header/Header';
 import './App.css';
+import Header from './components/Header/Header';
+import TeslaBatteryContainer from './containers/TeslaBatteryContainer';
 import appReducer from './reducers/teslaRangeApp';
 
 const store = createStore(appReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
@@ -18,31 +12,13 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-      <div>
-        <Header />
-        <div className="wrapper">
-          <form className="tesla-battery">
-            <h1>Range Per Charge</h1>
-            <TeslaCarContainer />
-            <TeslaStatsContainer />
-            <div className="tesla-controls cf">
-              <TeslaSpeedCounterContainer />
-              <div className="tesla-climate-container cf">
-                <TeslaTempCounterContainer />
-                <TeslaClimateContainer />
-              </div>
-              <TeslaWheelsContainer />
-            </div>   
-            <TeslaNotice />
-          </form>
+        <div>
+          <Header />
+          <TeslaBatteryContainer />
         </div>
-      </div>
       </Provider>
     );
   }
 }
 
 export default App;
-
-
-
